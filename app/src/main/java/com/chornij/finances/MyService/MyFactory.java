59655d1abde6fad4c0f7603cf855de2a69870fc9
Blackman <chornij.i.v@gmail.com>
2015-06-02 11:14:31 +0300

    Create widget

---
+++ app/src/main/java/com/chornij/finances/MyService/MyFactory.java
@@ -0,0 +1,120 @@
+package com.chornij.finances.MyService;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.io.IOException;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
+
+
+import com.chornij.finances.R;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MyFactory implements RemoteViewsFactory {
+
+    public static final String TAG = "----------->";
+
+    ArrayList<String> data;
+    Context context;
+    SimpleDateFormat sdf;
+    int widgetID;
+
+    MyFactory(Context ctx, Intent intent) {
+        context = ctx;
+        sdf = new SimpleDateFormat("HH:mm:ss");
+        widgetID = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public void onCreate() {
+        data = new ArrayList<String>();
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews rView = new RemoteViews(context.getPackageName(), R.layout.item);
+        rView.setTextViewText(R.id.tvItemText, data.get(position));
+
+        return rView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    OkHttpClient client = new OkHttpClient();
+
+
+    @Override
+    public void onDataSetChanged() {
+        data.clear();
+
+        String forecastUrl = "http://192.168.1.20/api/finances";
+
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jObject = new JSONObject(response.body().string());
+
+            JSONArray jArray = jObject.getJSONArray("sources");
+
+            for (int i = 0; i < jArray.length(); i++) {
+                try {
+                    JSONObject incomeSource = jArray.getJSONObject(i);
+
+                    Log.d(TAG, " ==== " + incomeSource.getString("name"));
+                    data.add(i, incomeSource.getString("name") + " - " + incomeSource.getString("amount"));
+                } catch (JSONException e) {
+                    Log.d(TAG, "JSONException: " + e.toString());
+                }
+            }
+
+            Log.d(TAG, response.body().string());
+        } catch (IOException e) {
+            Log.d(TAG, "IOException: " + e.toString());
+        } catch (JSONException e) {
+            Log.d(TAG, "JSONException: " + e.toString());
+        }
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+}
